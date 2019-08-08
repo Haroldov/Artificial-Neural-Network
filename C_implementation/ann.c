75d01d48d11a84d791a0a8fc1d0212e5b2999614
Haroldo Vélez <hvelez020@gmail.com>
2019-08-08 02:24:01 +0000

    [add] beginning of ann

---
+++ C_implementation/ann.c
@@ -0,0 +1,8 @@
+/**
+ *train - Train an Artificial Neural Network
+ *@data: matrix of data where rows are samples and columns are features
+ *@label: matrix of labels where rows are samples and there is one column
+ *@layer_size: number of neurons in the layer
+ *Return: array of weights trained for the hidden and ouput layer
+ */
+
